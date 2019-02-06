542433f4bd3dbc80d0c566c36702f936e5d9dda7
brunocaramelo <bruno.caramelo5@gmail.com>
2019-02-06 12:32:50 -0200

    adicao de campo presentation

--- application/database/migrations/2019_02_06_020148_create_authors_table.php
+++ application/database/migrations/2019_02_06_020148_create_authors_table.php
@@ -16,7 +16,7 @@ class CreateAuthorsTable extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('second_name');
+            $table->string('presentation');
             $table->timestamps();
         });
     }
