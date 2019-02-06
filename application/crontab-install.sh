9c4ac6d1a2c166f2eba474947379542bef1d867f
brunocaramelo <bruno.caramelo5@gmail.com>
2019-02-06 02:17:04 -0200

    added migration structure

--- application/crontab-install.sh
+++ application/crontab-install.sh
@@ -3,4 +3,8 @@ echo "Instaling crontab...";
 echo "* * * * * . php /var/www/html/artisan schedule:run >> /dev/null 2>&1" > /tmp/crontab
 crontab /tmp/crontab
 echo "Running composer install...";
-cd /var/www/html/ && composer install
+cd /var/www/html/ && composer install;
+
+cp /var/www/html/.env.example /var/www/html/.env
+
+php /var/www/html/artisan key:generate;
