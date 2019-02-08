09d50b165054c3e04f2460afcc2d0fee0ee3907a
brunocaramelo <bruno.caramelo5@gmail.com>
2019-02-08 13:37:20 -0200

    inicio do contexto books

---
+++ application/app/Domain/Books/Repositories/BookRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Domain\Authors\Repositories;
+
+use App\Domain\Books\Entities\BookEntity;
+use App\Infrastructure\Contracts\BaseRepositoryContract;
+
+class BookRepository implements BaseRepositoryContract
+{
+    private $book = null;
+
+    public function __construct(BookEntity $book)
+    {
+        $this->book = $book;
+    }
+
+    public function getAll()
+    {
+        return $this->book->all();
+    }
+
+    public function find($identify)
+    {
+        return $this->book->find($identify);
+    }
+
+    public function create($data)
+    {
+        return $this->book->create($data);
+    }
+
+    public function update($identify, $data)
+    {
+        $bookSave = $this->book->find($identify);
+        return $bookSave->fill($data)->save();
+    }
+}
