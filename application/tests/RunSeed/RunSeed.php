10684196893049dd9fb7df9f356709f256a9386a
brunocaramelo <bruno.caramelo5@gmail.com>
2019-02-10 10:58:39 -0200

    build automatico ajustado

--- application/tests/RunSeed/RunSeed.php
+++ application/tests/RunSeed/RunSeed.php
@@ -3,11 +3,14 @@
 namespace Tests\RunSeed;
 
 use Illuminate\Support\Facades\Artisan as Artisan;
+use Illuminate\Support\Facades\Cache as Cache;
+
 
 trait RunSeed
 {
     public function runSeed()
     {
+        Cache::flush();
         Artisan::call('db:seed', ['--class' => 'Seeds\ImporFakeJsonSeeder' ]);
     }
 }
