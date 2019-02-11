3d8a51ac8ce4941c333431f4bec45b629dd5af1f
brunocaramelo <bruno.caramelo5@gmail.com>
2019-02-11 20:26:29 -0200

    add service provader

--- application/app/Domain/Books/Controllers/BooksController.php
+++ application/app/Domain/Books/Controllers/BooksController.php
@@ -14,72 +14,55 @@ use App\Domain\Disciplines\Exceptions\DisciplineNotFoundException;
 
 class BooksController extends Controller
 {
-    public function getAll()
+    public function getAll(BookService $bookService)
     {
-        $bookService = new BookService();
         return response()->json(['data' => $bookService->getAll()], 200, [], JSON_NUMERIC_CHECK);
     }
     
-    public function getById(Request $request)
+    public function getById(BookService $bookService, Request $request)
     {
         try {
-            $bookService = new BookService();
             return response()->json(['data' => $bookService->getById($request->id)], 200, [], JSON_NUMERIC_CHECK);
         } catch (BookNotFoundException $error) {
             return response()->json(['error'=>$error->getMessage()], 404);
         }
     }
     
-    public function store(Request $request)
+    public function store(BookService $bookService, Request $request)
     {
         try {
             \DB::beginTransaction();
             $body = json_decode($request->getContent(), true);
-            $bookService = new BookService();
             $bookService->update($request->route('id'), $body);
             \DB::commit();
             return response()->json(['message'=>'Livro Editado com sucesso']);
-        } catch (BookEditException $error) {
+        } catch (BookEditException | AuthorNotFoundException | DisciplineNotFoundException $error) {
             \DB::rollback();
             return response()->json(['error'=>$error->getMessage()], 422);
         } catch (BookNotFoundException $error) {
             \DB::rollback();
             return response()->json(['error'=>$error->getMessage()], 404);
-        } catch (AuthorNotFoundException $error) {
-            \DB::rollback();
-            return response()->json(['error'=>$error->getMessage()], 422);
-        } catch (DisciplineNotFoundException $error) {
-            \DB::rollback();
-            return response()->json(['error'=>$error->getMessage()], 422);
         }
     }
 
-    public function create(Request $request)
+    public function create(BookService $bookService, Request $request)
     {
         try {
             \DB::beginTransaction();
             $body = json_decode($request->getContent(), true);
-            $bookService = new BookService();
             $bookService->create($body);
             \DB::commit();
             return response()->json(['message'=>'Livro Criado com sucesso']);
-        } catch (BookEditException $error) {
-            \DB::rollback();
-            return response()->json(['error'=>$error->getMessage()], 422);
-        } catch (AuthorNotFoundException $error) {
-            \DB::rollback();
-            return response()->json(['error'=>$error->getMessage()], 422);
-        } catch (DisciplineNotFoundException $error) {
+        } catch (BookEditException | AuthorNotFoundException | DisciplineNotFoundException $error) {
             \DB::rollback();
             return response()->json(['error'=>$error->getMessage()], 422);
         }
     }
 
-    public function remove(Request $request)
+    public function remove(BookService $bookService, Request $request)
     {
         try {
             \DB::beginTransaction();
-            $bookService = new BookService();
             $bookService->remove($request->id);
             \DB::commit();
             return response()->json(['data' => 'Livro Removido com sucesso'], 200);
