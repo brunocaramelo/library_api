d861300716ebb8a957637b13c9a1f68319b3d957
Bruno Caramelo <bruno.caramelo@ftd.com.br>
2020-09-02 22:39:08 -0300

    logs para schedullers

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Config;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +25,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            \Log::error('ta rodando: '.date('H:i:s'));
+            \Log::warning('ta rodando: '.date('H:i:s'));
+            \Log::info('ta rodando: '.date('H:i:s'));
+
+        })->everyFiveMinutes();
     }
 
     /**
