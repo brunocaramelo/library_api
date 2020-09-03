08fa7a9751fe6ccccdae9d608a8de246bfd9f44d
Bruno Caramelo <bruno.caramelo@ftd.com.br>
2020-09-03 12:10:03 -0300

    ajustes

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -26,11 +26,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
+
+            \Log::info(json_encode([get_current_user(),env('LOG_CHANNEL'),env('SESSION_DRIVER'),env('DB_DATABASE')]))
             \Log::error('ta rodando: '.date('H:i:s'));
             \Log::warning('ta rodando: '.date('H:i:s'));
             \Log::info('ta rodando: '.date('H:i:s'));
 
-        })->everyFiveMinutes();
+        })->everyMinute();
     }
 
     /**
